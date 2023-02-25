cfe372e375ad23855f9bd2d04830ffc445edad1d
Solomon Apam <kingsolomon0240@gmail.com>
2023-02-25 07:50:08 -0800

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print "with proper grammar, but the outcome is
+ * 		a peice of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a peice of art,\n");
+	
+	return (0);
+}
